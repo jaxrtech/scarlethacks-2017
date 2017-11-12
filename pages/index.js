0b02f8ff1ebd9aa77d01fefaabab448b56eb492f
Josh Bowden <josbow@gmail.com>
2017-11-11 18:35:15 -0600

    chore: refactor app bar to `_document.tsx` template

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
-const head_1 = require("next/head");
-const styles_1 = require("material-ui/styles");
-const AppBar_1 = require("../components/AppBar");
+const link_1 = require("next/link");
 exports.default = () => React.createElement("div", null,
-    React.createElement(head_1.default, null,
-        React.createElement("title", null, "My page title"),
-        React.createElement("meta", { name: "viewport", content: "initial-scale=1.0, width=device-width" })),
-    React.createElement("style", { global: true, jsx: true }, `
-      html, body {
-        padding: 0;
-        margin: 0;
-      }
-    `),
-    React.createElement(styles_1.MuiThemeProvider, null,
-        React.createElement(AppBar_1.AppBar, null)));
+    React.createElement("p", null, "Hi Noah!"),
+    React.createElement(link_1.default, { href: "/about" },
+        React.createElement("a", null, "About Page")));
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,6 @@ import { AppBar } from '../components/AppBar';
 
 export default () =>
   <div>
-    <Head>
-      <title>My page title</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <style global jsx>{`
-      html, body {
-        padding: 0;
-        margin: 0;
-      }
-    `}</style>
-    <MuiThemeProvider>
-      <AppBar/>
-    </MuiThemeProvider>
+    <p>Hi Noah!</p>
+    <Link href="/about"><a>About Page</a></Link>
   </div>
